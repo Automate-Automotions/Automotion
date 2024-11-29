e99bcbab7ee61a0bfeb012c69e7765e574a898e1
Bichal <vtorresnun@gmail.com>
2024-11-29 17:13:48 +0100

    Automotion principal actualizado

--- src/main/java/net/bichal/automotion/Automotion.java
+++ src/main/java/net/bichal/automotion/Automotion.java
@@ -3,7 +3,6 @@ package net.bichal.automotion;
 import net.bichal.automotion.block.ModBlocks;
 import net.bichal.automotion.item.ModItemGroups;
 import net.bichal.automotion.item.ModItems;
-import net.bichal.automotion.util.ModLootTableModifiers;
 import net.bichal.automotion.world.gen.ModWorldGeneration;
 import net.fabricmc.api.ModInitializer;
 
@@ -12,19 +11,11 @@ import org.slf4j.LoggerFactory;
 
 public class Automotion implements ModInitializer {
     public static final String MOD_ID = "automotion";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = LoggerFactory.getLogger("Automotion");
 
     @Override
     public void onInitialize() {
-        LOGGER.info("""
-                
-                #############################################################
-                
-                            Automotion mod has been initialized!
-                
-                #############################################################
-                """);
-        ModLootTableModifiers.modifyLootTables();
+        LOGGER.info("[Automotion] Has been initialized!");
         ModWorldGeneration.generateModWorldGen();
 
         ModItemGroups.registerModItemGroups();
